dd4195f5802fbfb62bf1209ae35de1822d6f14a5
Branko Milosavljevic <mbranko@uns.ac.rs>
2019-06-06 15:49:09 +0200

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MDBBootstrapModulesPro.forRoot(),
   ],
-  providers: [],
+  providers: [
+    MDBSpinningPreloader,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
