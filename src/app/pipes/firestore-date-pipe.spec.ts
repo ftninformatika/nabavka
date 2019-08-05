2c4434fd674b720482bb107c8b313da9a0fe6bf7
danijela <tesendic>
2019-08-05 12:19:36 +0200

    acquisition in progress

--- src/app/pipes/firestore-date-pipe.spec.ts
+++ src/app/pipes/firestore-date-pipe.spec.ts
@@ -2,6 +2,7 @@ import { FirestoreDatePipe } from './firestore-date-pipe';
 
 describe('FirestoreDatePipe', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new FirestoreDatePipe()).toBeTruthy();
   });
 });
